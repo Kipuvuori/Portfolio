ac2df661b5073b21afb5e3718f7634fe75f653ec
Anton Lehmus <anton.lehmus@gmail.com>
2016-10-23 11:52:54 +0300

    add db seed to prepopulate site_infos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+default_site_infos = [{ name: 'page_title', value:'Title for the portfolio page.' },
+                      { name: 'personal_info_title', value:'Title for the personal info box.' },
+                      { name: 'photo' , value:'Photo 	Filename for the photo in public/images/ to use as portfolio\'s photo.' },
+                      { name: 'photo_description' , value:'Description text for the photo.' }]
+
+default_site_infos.each do |info|
+  SiteInfo.find_or_create_by(name: info[:name]) do |default_info|
+    default_info.value = info[:value]
+  end
+end
